088a40ddc36f69104f7a7f5f81ac7d60f376de3d
lshh18295801914 <874638140@qq.com>
2019-07-16 16:11:22 +0800

    add check cookies

--- src/main/java/com/lvshh/community/mapper/UserMapper.java
+++ src/main/java/com/lvshh/community/mapper/UserMapper.java
@@ -3,10 +3,15 @@ package com.lvshh.community.mapper;
 import com.lvshh.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 
 @Mapper
 public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{account_id},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token=#{token}")
+    User findByToken(@Param("token") String token);
 }
